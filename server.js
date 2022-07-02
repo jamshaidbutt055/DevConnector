04934aef2cf868947d4cfdf153db7ffae30fca4d
rizwan-ijaz-056 <jamshaidbutt055@gmail.com>
2022-07-02 16:57:49 +0500

    Added Routes

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express")
-
+const connectDB = require("./config/db")
 const APP = express()
 const PORT = process.env.PORT || 5000
 
+connectDB()
+
 APP.get("/", (req, res) => res.send("API Called"))
+APP.use("/api/auth", require("./routes/api/auth"))
+APP.use("/api/users", require("./routes/api/users"))
+APP.use("/api/profile", require("./routes/api/profile"))
+APP.use("/api/posts", require("./routes/api/posts"))
 APP.listen(PORT, () => console.log(`Server is running on Port ${PORT}.`))
