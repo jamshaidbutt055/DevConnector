0853a794280ddffd69a6a5c28c7330a93a1585bf
rizwan-ijaz-056 <jamshaidbutt055@gmail.com>
2022-07-13 23:11:30 +0500

    error msg

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     await mongoose.connect(db)
     console.log("MongoDB Connected!")
   } catch (err) {
-    console.log(err)
+    console.log(err.message)
     process.exit(1)
   }
 }
